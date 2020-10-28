22b35d7007cf144e13f2ffb3293ec8824c618b87
hanool <29415530+hanool@users.noreply.github.com>
2020-10-28 23:03:46 +0900

    Updates

---
+++ precache-manifest.9001eee70f44c3bdcaf57f2a6c3a56c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa3fd40d39783a8ac475d43d46b6367a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bf5427ad279c797b9b1",
+    "url": "/static/css/main.d84d1b0a.chunk.css"
+  },
+  {
+    "revision": "e7d54182cbc031fbb42c",
+    "url": "/static/js/2.72a22a35.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.72a22a35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bf5427ad279c797b9b1",
+    "url": "/static/js/main.9576f138.chunk.js"
+  },
+  {
+    "revision": "1eaf62d46a4a10f36e6e",
+    "url": "/static/js/runtime-main.d3562609.js"
+  }
+]);
